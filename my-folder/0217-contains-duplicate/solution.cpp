d2e363a51ca6f5efc4578dbfa1b51fded7701b05
Sarthak Kashyap <98842340+ROOKIEsarthak@users.noreply.github.com>
2026-04-12 09:26:29 +0000

    [LeetCode Sync] Runtime - 55 ms (31.80%), Memory - 90.9 MB (44.65%)

---
+++ my-folder/0217-contains-duplicate/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> us;
+        for(int num : nums){
+            if(us.count(num)){
+                return true;
+            }
+            us.insert(num);
+        }return false;
+    }
+};
+
+
