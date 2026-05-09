ae1f155c98fe4bfee22687a5d080cd79ab48a68d
Sarthak Kashyap <98842340+ROOKIEsarthak@users.noreply.github.com>
2026-05-09 08:17:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.2 MB (70.95%)

--- my-folder/0088-merge-sorted-array/solution.cpp
+++ my-folder/0088-merge-sorted-array/solution.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int l = m+n , r = n , val = 0;
-        while(r > 0){
-            if(l == 0){
-                nums1[0] = nums2[r-1];
-                r--;
-            }else{ 
-                nums1[l-1] = nums2[r-1];
+        int l = m-1 , r = n-1 , tl = nums1.size()-1;
+        // if arr1 size is 0 and arr2 size is n then push all the elements of arr2 into arr1;
+        // if arr1 size is m and arr2 size is 0 then nothing to merge do nothing;
+        // if both arr1 and arr2 have elements m and n then 
+            // iterate from back of arr1 as it is of size (m+n)
+            // if(nums1[l]<nums2[r]){
+            //     nums1[tl] = nums2[r];
+            //     r--; // move the arr2 pointer to the left 
+            // }else{
+            //     nums1[tl] = nums1[l];
+            //     nums1[l] = 0;
+            //     l--;
+            // }
+            
+        while (r >= 0) {
+            if (l >= 0 && nums1[l] > nums2[r]) {
+                nums1[tl] = nums1[l];
                 l--;
+            } else {
+                nums1[tl] = nums2[r];
                 r--;
             }
+            tl--;
         }
-        sort(nums1.begin(),nums1.end());
     }
 };
